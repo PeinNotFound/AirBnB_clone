228df383a7a561439ce1c1d48754c2bb11ee678d
PeinNotFound <akatsuki_ahmed@yahoo.com>
2024-03-07 16:16:37 +0100

    adding kwargs and if statements and time format

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,21 @@ from datetime import datetime
 class BaseModel:
     """Base class for models with basic methods."""
 
-    def __init__(self):
-        """Initialize instance with ids, timestamps, and optional attributes."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    def __init__(self, *args, **kwargs):
+        time_format = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            """Initialize instance with ids, timestamps, and optional attributes."""
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """Update 'updated_at' timestamp and save the instance"""
