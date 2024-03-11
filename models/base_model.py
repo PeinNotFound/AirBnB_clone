6fa5b1e9dba8c8243ca79eb1cddfdd2eec270393
PeinNotFound <akatsuki_ahmed@yahoo.com>
2024-03-11 12:11:37 +0000

    testing

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 '''The BaseModel defines all common attributes/methods for other classes.'''
 import uuid
 from datetime import datetime
 import AirBnB_clone.models
 
+
 class BaseModel:
     '''Base class for models with basic methods.'''
-
     def __init__(self, *args, **kwargs):
-        time_format = '%Y-%m-%dT%H:%M:%S.%f'
-
-        '''Initialize instance with ids, timestamps, and optional attributes.'''
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
@@ -24,13 +22,13 @@ class BaseModel:
                 else:
                     setattr(self, key, value)
 
-        models.storage.new(self)
+        AirBnB_clone.models.storage.new(self)
 
     def save(self):
         '''Update 'updated_at' timestamp and save the instance'''
         self.updated_at = datetime.utcnow()
-        models.storage.save()
-    
+        AirBnB_clone.models.storage.save()
+
     def to_dict(self):
         '''Convert attributes to a dictionary with class info and timestamps'''
         inst_dict = self.__dict__.copy()
@@ -43,18 +41,28 @@ class BaseModel:
     def __str__(self):
         '''returns a string representation: '[ClassName] (id) attributes'''
         class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id,self.__dict__)
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     my_model = BaseModel()
-    my_model.name = "My First Model"
+    my_model.name = "My_First_Model"
     my_model.my_number = 89
+    print(my_model.id)
     print(my_model)
-    my_model.save()
-    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
